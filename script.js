03fcd75b75db8c85deba5f2fa50a8ae9758736aa
letaljack <138536528+letaljack@users.noreply.github.com>
2025-03-23 19:44:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 // Inicializar AOS (Animaciones de Scroll)
 AOS.init({
-    duration: 1000,
-    once: true,
-    offset: 120
-  });
-  
-  // Deshabilitar clic derecho
-  document.addEventListener('contextmenu', function(e) {
+  duration: 1000,
+  once: true,
+  offset: 120
+});
+
+// Deshabilitar clic derecho
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+});
+
+// Implementar scroll suave para enlaces de navegación
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
     e.preventDefault();
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
   });
-  
-  // Implementar scroll suave para enlaces de navegación
-  document.querySelectorAll('a.nav-link').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      const targetId = this.getAttribute('href');
-      const targetElement = document.querySelector(targetId);
-      if (targetElement) {
-        targetElement.scrollIntoView({ behavior: 'smooth' });
-      }
-    });
-  });
-  
+});
